b0135d43d370a2ee8d43762901751fba2dea3ddb
usselman <sarkar.shirish@gmail.com>
2023-12-04 10:19:33 -0800

    Null super fix

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -68,7 +68,7 @@ export const HomePage = () => {
     );
     return (
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {ordinals.map((ordinal, index) => (
+        {filteredOrdinals.map((ordinal, index) => (
           <OrdinalCard key={index} ordinal={ordinal} />
         ))}
       </div>
